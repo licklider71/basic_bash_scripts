bf050c0f1c7aa924602ee4f06dc2253b8f510d37
licklider71 <94425861+licklider71@users.noreply.github.com>
2022-07-23 07:12:54 -0400

    Update install-vmware-tools.sh

--- install-vmware-tools.sh
+++ install-vmware-tools.sh
@@ -3,15 +3,9 @@ install vmware tools in linux virtual guest */
 cd ~
 sudo
 mkdir /mnt/cdrom 
-&& 
 mount /dev/cdrom 
-&&
 cp /mnt/cdrom/VMwareTools-version.tar.gz /tmp/
-&&
 cd /tmp
-&&
 tar -zxvf VMwareTools-version.tar.gz
-&&
 cd vmware-tools-distrib
-&&
 ./vmware-install.pl
